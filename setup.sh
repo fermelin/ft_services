734f1f0b93ea830c7e7ae97bafd3f0eff3827224
Floria Ermelinda <fermelin@il-a3.msk.21-school.ru>
2021-03-04 17:51:20 +0300

    probably the final version of the project

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-function echo_green
+function echo_blue
 {
-	echo "\033[1;42m$1\033[0m"
+	echo -e "\033[1;34;47m		$1		\033[0m"
 }
 
 function echo_red
 {
-	echo "\033[1;41m$1\033[0m"
+	echo -e "\033[1;31;47m		$1		\033[0m"
 }
 
 function error
@@ -19,16 +19,22 @@ function error
 
 function build_image 
 {
-	echo_green "--BUILDING $2 IMAGE--"
+	echo_blue "--BUILDING $2 IMAGE--"
 
 	attempts=0
-	until [ $? -eq 0 ] | [ $attempts -eq 3 ]
+	exit_status=0
+	until [ $attempts -eq 3 ]
 	do
 		$1
+		exit_status=$?
+		if [ $exit_status -eq 0 ]
+		then
+			break
+		fi
 		((attempts=attempts+1))
 	done
 
-	if [ $? -ne 1 ]
+	if [ $exit_status -ne 0 ]
 	then
 		error $2
 	fi
@@ -90,71 +96,3 @@ kubectl apply -f ./srcs/k8s_telegraf_daemon/
 build_image "docker build -t grafana_image ./srcs/grafana" "GRAFANA"
 kubectl apply -f ./srcs/k8s_deployments/grafana_deployment.yaml
 kubectl apply -f ./srcs/k8s_services/grafana_service.yaml
-
-#______________________________________________________________________________________
-
-# minikube start --vm-driver=virtualbox
-
-# minikube dashboard &
-
-# minikube addons enable metallb
-# minikube addons enable metrics-server
-
-# kubectl apply -f ./srcs/configmap.yaml
-
-# kubectl create secret generic -n metallb-system memberlist \
-#     --from-literal=secretkey="$(openssl rand -base64 128)"
-
-# export MINIKUBE_IP=$(minikube ip)
-
-# eval $(minikube docker-env) && sleep 3
-
-# printf "\nNGINX..."
-# docker build -t nginx_image ./srcs/nginx
-# echo "Ok"
-# kubectl apply -f ./srcs/k8s_deployments/nginx_deployment.yaml
-# kubectl apply -f ./srcs/k8s_services/nginx_service.yaml
-
-# printf "\nMYSQL..."
-# docker build -t mysql_image ./srcs/mysql
-# echo "Ok"
-# kubectl apply -f ./srcs/k8s_volumes/mysql_volume.yaml
-# kubectl apply -f ./srcs/k8s_deployments/mysql_deployment.yaml
-# kubectl apply -f ./srcs/k8s_services/mysql_service.yaml
-
-# printf "\nFTPS..."
-# docker build -t ftps_image ./srcs/ftps
-# echo "Ok"
-# kubectl apply -f ./srcs/k8s_volumes/ftps_volume.yaml
-# kubectl apply -f ./srcs/k8s_deployments/ftps_deployment.yaml
-# kubectl apply -f ./srcs/k8s_services/ftps_service.yaml
-
-# printf "\nPHPMYADMIN..."
-# docker build -t phpmyadmin_image ./srcs/phpmyadmin
-# echo "Ok"
-# kubectl apply -f ./srcs/k8s_deployments/phpmyadmin_deployment.yaml
-# kubectl apply -f ./srcs/k8s_services/phpmyadmin_service.yaml
-
-# printf "\nWORDPRESS..."
-# docker build -t wordpress_image ./srcs/wordpress
-# echo "Ok"
-# kubectl apply -f ./srcs/k8s_deployments/wordpress_deployment.yaml
-# kubectl apply -f ./srcs/k8s_services/wordpress_service.yaml
-
-# printf "\nINFLUXDB..."
-# docker build -t influxdb_image ./srcs/influxdb
-# echo "Ok"
-# kubectl apply -f ./srcs/k8s_volumes/influxdb_volume.yaml
-# kubectl apply -f ./srcs/k8s_deployments/influxdb_deployment.yaml
-# kubectl apply -f ./srcs/k8s_services/influxdb_service.yaml
-
-# printf "\nTELEGRAF..."
-# docker build --build-arg MINIKUBE_IP=${MINIKUBE_IP} -t telegraf_image ./srcs/telegraf
-# echo "Ok"
-# kubectl apply -f ./srcs/k8s_telegraf_daemon/
-
-# printf "\nGRAFANA..."
-# docker build -t grafana_image ./srcs/grafana
-# echo "Ok"
-# kubectl apply -f ./srcs/k8s_deployments/grafana_deployment.yaml
-# kubectl apply -f ./srcs/k8s_services/grafana_service.yaml
